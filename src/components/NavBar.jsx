0c222f0d3d0e979d69c1a6e34a45aa439b878db9
Karolina Marszal <karomarszal@gmail.com>
2023-04-20 12:26:44 +0200

    Added NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import classNames from "../../utils/helpers";
+import logo from "../../public/logo.png";
+
+const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+  
+  useEffect(()=> {
+    const onScroll = () => {
+      if(window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="Logo"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={classNames ({
+              "active navbar-link": activeLink === "home", 
+              "navbar-link": activeLink !== "home",
+              "text-scrolled": scrolled,
+              "": !scrolled,
+            })} 
+            onClick={()=> onUpdateActiveLink("home")}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={classNames ({
+              "active navbar-link": activeLink === "skills", 
+              "navbar-link": activeLink !== "skills",
+              "text-scrolled": scrolled,
+              "": !scrolled,
+            })}  
+            onClick={()=> onUpdateActiveLink("skills")}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={classNames ({
+              "active navbar-link": activeLink === "projects", 
+              "navbar-link": activeLink !== "projects",
+              "text-scrolled": scrolled,
+              "": !scrolled,
+            })} 
+            onClick={()=> onUpdateActiveLink("projects")}>Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <button className="vvd" onClick={()=> console.log("connect")}><span>Let's Connect</span></button>
+          </span>
+          </Navbar.Collapse> 
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
