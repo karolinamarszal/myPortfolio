be3781fc3c1adae2f7bc88978e0f6c28e69c73ff
Karolina Marszal <karomarszal@gmail.com>
2023-04-25 17:02:27 +0200

    Updated typo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,7 +35,7 @@ const Projects = ()=> {
     },
   ]
   return (
-    <section className="project" id="project">
+    <section className="project" id="projects">
       <Container>
         <Row>
           <Col>
