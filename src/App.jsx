d0b54dfebad1983c3573f7aba9d7253cba8f0d11
Karolina Marszal <karomarszal@gmail.com>
2023-04-27 21:29:34 +0200

    Updated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <h2>TEST</h2>
+        <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
